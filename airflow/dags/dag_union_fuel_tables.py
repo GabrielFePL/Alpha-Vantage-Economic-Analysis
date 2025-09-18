c26d3724f6d39cf61711b56160f4798fdf15c78b
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2025-09-18 17:16:10 -0300

    Fuel SIlver Layer

---
+++ airflow/dags/dag_union_fuel_tables.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="dag_union_tables",
+    default_args=default_args,
+    description="Faz UNION de 3 tabelas no SQL Server",
+    schedule=None,
+    start_date=datetime(2025, 9, 18),
+    catchup=False,
+    tags=["sqlserver", "union"],
+) as dag:
+
+    union_query = """
+    SELECT name, interval, unit, date, value
+    INTO silver_fuel FROM
+(
+	SELECT name, interval, unit, date, value FROM bronze_natural_gas
+	UNION
+	SELECT name, interval, unit, date, value FROM bronze_oil_wti
+	UNION
+	SELECT name, interval, unit, date, value FROM bronze_oil_brent
+) AS silver_fuel;
+    """
+
+    union_task = MsSqlOperator(
+        task_id="union_task",
+        mssql_conn_id="mssql_default",  # conexão criada no Airflow UI
+        sql=union_query,
+    )
+
+    union_task
